efe7fa257b04b4f08a0309e90c87904353b47f4c
jizhiyuan <jizhiyuan@bytedance.com>
2025-07-03 11:05:05 +0800

    add rv64i instdb
    update

--- src/asmjit/riscv/riscvemitter.h
+++ src/asmjit/riscv/riscvemitter.h
@@ -9,6 +9,7 @@
 #include "../core/emitter.h"
 #include "../core/support.h"
 #include "../riscv/riscvoperand.h"
+#include "../riscv/riscvinstdb.h"
 
 ASMJIT_BEGIN_SUB_NAMESPACE(riscv)
 
@@ -29,7 +30,64 @@ struct EmitterExplicitT {
 
   // RV64I Base Instruction Set
   ASMJIT_INST_3x(add, Add, Gp, Gp, Gp)
+  ASMJIT_INST_3x(addw, Addw, Gp, Gp, Gp)
+  ASMJIT_INST_3x(sub, Sub, Gp, Gp, Gp)
+  ASMJIT_INST_3x(subw, Subw, Gp, Gp, Gp)
+  ASMJIT_INST_3x(sll, Sll, Gp, Gp, Gp)
+  ASMJIT_INST_3x(sllw, Sllw, Gp, Gp, Gp)
+  ASMJIT_INST_3x(srl, Srl, Gp, Gp, Gp)
+  ASMJIT_INST_3x(srlw, Srlw, Gp, Gp, Gp)
+  ASMJIT_INST_3x(sra, Sra, Gp, Gp, Gp)
+  ASMJIT_INST_3x(sraw, Sraw, Gp, Gp, Gp)
+  ASMJIT_INST_3x(slt, Slt, Gp, Gp, Gp)
+  ASMJIT_INST_3x(sltu, Sltu, Gp, Gp, Gp)
+  ASMJIT_INST_3x(and_, And, Gp, Gp, Gp)
+  ASMJIT_INST_3x(or_, Or, Gp, Gp, Gp)
+  ASMJIT_INST_3x(xor_, Xor, Gp, Gp, Gp)
+
   ASMJIT_INST_3x(addi, Addi, Gp, Gp, Imm)
+  ASMJIT_INST_3x(addiw, Addiw, Gp, Gp, Imm)
+  ASMJIT_INST_3x(slti, Slti, Gp, Gp, Imm)
+  ASMJIT_INST_3x(sltiu, Sltiu, Gp, Gp, Imm)
+  ASMJIT_INST_3x(andi, Andi, Gp, Gp, Imm)
+  ASMJIT_INST_3x(ori, Ori, Gp, Gp, Imm)
+  ASMJIT_INST_3x(xori, XOri, Gp, Gp, Imm)
+  ASMJIT_INST_3x(slli, Slli, Gp, Gp, Imm)
+  ASMJIT_INST_3x(slliw, Slliw, Gp, Gp, Imm)
+  ASMJIT_INST_3x(srli, Srli, Gp, Gp, Imm)
+  ASMJIT_INST_3x(srliw, Srliw, Gp, Gp, Imm)
+  ASMJIT_INST_3x(srai, Srai, Gp, Gp, Imm)
+  ASMJIT_INST_3x(sraiw, Sraiw, Gp, Gp, Imm)
+  ASMJIT_INST_3x(jalr, Jalr, Gp, Gp, Imm)
+
+  ASMJIT_INST_2x(lb, Lb, Gp, Mem)
+  ASMJIT_INST_2x(lh, Lh, Gp, Mem)
+  ASMJIT_INST_2x(lw, Lw, Gp, Mem)
+  ASMJIT_INST_2x(lbu, Lbu, Gp, Mem)
+  ASMJIT_INST_2x(lhu, Lhu, Gp, Mem)
+  ASMJIT_INST_2x(lwu, Lwu, Gp, Mem)
+  ASMJIT_INST_2x(ld, Ld, Gp, Mem)
+
+  ASMJIT_INST_2x(sb, Sb, Mem, Gp)
+  ASMJIT_INST_2x(sh, Sh, Mem, Gp)
+  ASMJIT_INST_2x(sw, Sw, Mem, Gp)
+  ASMJIT_INST_2x(sd, Sd, Mem, Gp)
+
+  ASMJIT_INST_3x(beq, Beq, Gp, Gp, Label)
+  ASMJIT_INST_3x(bne, Bne, Gp, Gp, Label)
+  ASMJIT_INST_3x(blt, Blt, Gp, Gp, Label)
+  ASMJIT_INST_3x(bge, Bge, Gp, Gp, Label)
+  ASMJIT_INST_3x(bltu, Bltu, Gp, Gp, Label)
+  ASMJIT_INST_3x(bgeu, Bgeu, Gp, Gp, Label)
+
+  ASMJIT_INST_2x(lui, Lui, Gp, Imm)
+  ASMJIT_INST_2x(auipc, Auipc, Gp, Imm)
+
+  ASMJIT_INST_2x(jal, Jal, Gp, Label)
+
+  inline Error jal(const Label& o0) { return _emitter()->_emitI(Inst::kIdJal, regs::ra, o0); }
+  inline Error nop() { return _emitter()->_emitI(Inst::kIdNop); }
+  inline Error fence() { return _emitter()->_emitI(Inst::kIdFence); }
 
   // ... more instructions will be added here
 };
--- src/asmjit/riscv/riscvinstdb.h
+++ src/asmjit/riscv/riscvinstdb.h
@@ -19,14 +19,168 @@ namespace Inst {
 //! Instruction IDs (RISC-V).
 enum Id : uint32_t {
   kIdNone = 0,
+  // R-Type
   kIdAdd,
+  kIdAddw,
+  kIdSub,
+  kIdSubw,
+  kIdSll,
+  kIdSllw,
+  kIdSrl,
+  kIdSrlw,
+  kIdSra,
+  kIdSraw,
+  kIdSlt,
+  kIdSltu,
+  kIdAnd,
+  kIdOr,
+  kIdXor,
+  // I-Type
   kIdAddi,
+  kIdAddiw,
+  kIdSlti,
+  kIdSltiu,
+  kIdAndi,
+  kIdOri,
+  kIdXOri,
+  kIdSlli,
+  kIdSlliw,
+  kIdSrli,
+  kIdSrliw,
+  kIdSrai,
+  kIdSraiw,
+  kIdJalr,
+  // Load
+  kIdLb,
+  kIdLh,
+  kIdLw,
+  kIdLbu,
+  kIdLhu,
+  kIdLwu,
+  kIdLd,
+  // S-Type
+  kIdSb,
+  kIdSh,
+  kIdSw,
+  kIdSd,
+  // B-Type
+  kIdBeq,
+  kIdBne,
+  kIdBlt,
+  kIdBge,
+  kIdBltu,
+  kIdBgeu,
+  // U-Type
+  kIdLui,
+  kIdAuipc,
+  // J-Type
+  kIdJal,
+  // Others
+  kIdNop,
+  kIdFence,
   // ... more instructions will be added here
 
   kIdCount
 };
 
+//! Tests whether the instruction `id` is a valid and known instruction.
+[[nodiscard]]
+static ASMJIT_INLINE_NODEBUG bool isDefinedId(InstId id) noexcept {
+  return id > 0 && id < kIdCount;
+}
+
+[[nodiscard]]
+ASMJIT_INLINE_NODEBUG bool isRType(InstId id) {
+  return id >= kIdAdd && id <= kIdXor;
+}
+
+[[nodiscard]]
+ASMJIT_INLINE_NODEBUG bool isIType(InstId id) {
+  return id >= kIdAddi && id <= kIdSraiw;
+}
+
+[[nodiscard]]
+ASMJIT_INLINE_NODEBUG bool isSType(InstId id) {
+  return id >= kIdSb && id <= kIdSd;
+}
+
+[[nodiscard]]
+ASMJIT_INLINE_NODEBUG bool isBType(InstId id) {
+  return id >= kIdBeq && id <= kIdJalr;
+}
+
+[[nodiscard]]
+ASMJIT_INLINE_NODEBUG bool isUType(InstId id) {
+  return id >= kIdLui && id <= kIdAuipc;
+}
+
+[[nodiscard]]
+ASMJIT_INLINE_NODEBUG bool isJType(InstId id) {
+  return id >= kIdJal && id <= kIdJal;
+}
+
 } // {Inst}
+
+namespace InstDB {
+
+//! Instruction encoding type (RISC-V).
+enum class EncodingType : uint8_t {
+  kNone = 0,
+  kR,
+  kI,
+  kS,
+  kB,
+  kU,
+  kJ
+};
+
+//! Instruction flags (RISC-V).
+enum InstFlags : uint16_t {
+  kIsLoad = 0x0001u,
+  kIsStore = 0x0002u,
+  kIsJump = 0x0004u,
+  kIsBranch = 0x0008u
+};
+
+//! Instruction information (RISC-V).
+struct InstInfo {
+  //! Instruction encoding type.
+  uint32_t _encoding : 8;
+  //! Index to data specific to each encoding type.
+  uint32_t _encodingDataIndex : 8;
+  uint32_t _reserved : 16;
+
+  uint16_t _rwInfoIndex;
+  uint16_t _flags;
+
+  //! \name Accessors
+  //! \{
+
+  [[nodiscard]]
+  ASMJIT_INLINE_NODEBUG uint32_t rwInfoIndex() const noexcept { return _rwInfoIndex; }
+
+  [[nodiscard]]
+  ASMJIT_INLINE_NODEBUG uint32_t flags() const noexcept { return _flags; }
+
+  [[nodiscard]]
+  ASMJIT_INLINE_NODEBUG bool hasFlag(uint32_t flag) const { return (_flags & flag) != 0; }
+
+  [[nodiscard]]
+  ASMJIT_INLINE_NODEBUG EncodingType encoding() const noexcept { return (EncodingType)_encoding; }
+
+  //! \}
+};
+
+ASMJIT_VARAPI const InstInfo _instInfoTable[];
+
+[[nodiscard]]
+static inline const InstInfo& infoById(InstId instId) noexcept {
+  instId &= uint32_t(InstIdParts::kRealId);
+  ASMJIT_ASSERT(Inst::isDefinedId(instId));
+  return _instInfoTable[instId];
+}
+
+} // {InstDB}
 
 //! \}
 
